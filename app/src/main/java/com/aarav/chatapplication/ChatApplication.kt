a76f19655669ea4df1bdeba26ef854ccd32f2dd0
Aarav3325 <aaravhalvadia@gmail.com>
2026-02-10 13:56:48 +0530

    Completed PostHog setup

--- app/src/main/java/com/aarav/chatapplication/ChatApplication.kt
+++ app/src/main/java/com/aarav/chatapplication/ChatApplication.kt
@@ -1,6 +1,7 @@
 package com.aarav.chatapplication
 
 import android.app.Application
+import androidx.compose.ui.res.stringResource
 import com.posthog.android.PostHogAndroid
 import com.posthog.android.PostHogAndroidConfig
 import dagger.hilt.android.HiltAndroidApp
@@ -8,15 +9,15 @@ import dagger.hilt.android.HiltAndroidApp
 @HiltAndroidApp
 class ChatApplication: Application() {
     companion object {
-        const val POSTHOG_API_KEY = "phc_sefjPOesubDVrF0AWNGmAoca3qjX4Be6UfxKAinIBC1"
         const val POSTHOG_HOST = "https://us.i.posthog.com"
     }
 
     override fun onCreate() {
         super.onCreate()
 
+
         val config = PostHogAndroidConfig(
-            apiKey = POSTHOG_API_KEY,
+            apiKey = applicationContext.getString(R.string.posthog_api),
             host = POSTHOG_HOST
         )
         PostHogAndroid.setup(this, config)
